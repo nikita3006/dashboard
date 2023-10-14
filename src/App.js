e801ce411f4aaf09e45918f03747d0bb61fcc5f7
nikita3006 <nikitanegi3011@gmail.com>
2023-10-14 21:28:47 +0530

    fourthchange

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <main className='main'>
-      <section className={menu=== false ? 'dashboard-menu' : 'dashboard-menu toggle'}>
+      <section className={menu=== false ? 'dashboard-menu' : 'dashboard-menu toggle'}  onClick={menuHandler}>
         <div className='dashboard-menu-content'>
           <div className='dashboard-menu-header'>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m12 .845l9.66 5.578v11.154L12 23.155l-9.66-5.578V6.423L12 .845Zm0 2.31L4.34 7.577v8.846L12 20.845l7.66-4.422V7.577L12 3.155ZM12 9a3 3 0 1 0 0 6a3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0a5 5 0 0 1-10 0Z"/></svg>
